8b636f54f323774cdb35145e72173eaf5711d648
bastien <bastiencaumont@orange.fr>
2021-10-29 20:49:28 +0200

    mon message de commit

---
+++ src/test/java/com/ecommerce/microcommerce/MicrocommerceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ecommerce.microcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
+class MicrocommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//assertEquals(2,1);
+	}
+
+}
