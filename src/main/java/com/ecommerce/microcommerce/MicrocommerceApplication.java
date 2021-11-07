cb0010fb9c61ce2f535276d89ac3e9378c9a2921
bastien <bastiencaumont@orange.fr>
2021-11-07 13:15:36 +0100

    Ajout du Print-Suppresion Titi

--- src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -1,5 +1,6 @@
 package com.ecommerce.microcommerce;
 
+import org.omg.IOP.ServiceContextHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class MicrocommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicrocommerceApplication.class, args);
+		System.out.print("Je chante");
 	}
+
 
 }
